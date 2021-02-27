69ff36f930c4dbf4c992dd39c6639bb314e8b8eb
Martin Pata <mpata@fi.uba.ar>
2021-02-27 15:45:38 -0300

    Borrado de archivos que se movieron

--- archivos.h
+++
@@ -1,9 +0,0 @@
-#ifndef __ARCHIVOS_H__
-#define __ARCHIVOS_H__
-
-#include <stdio.h>
-#include "gimnasio.h"
-
-heap_t* lectura_archivo_gimnasios(const char* ruta_archivo);
-
-#endif /* __ARCHIVOS_H__ */
