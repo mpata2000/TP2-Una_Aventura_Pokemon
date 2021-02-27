723c71ce1e6587e16324dce936e9f533cd97c3fe
Martin Pata <mpata@fi.uba.ar>
2021-02-27 15:40:45 -0300

    Se agregaron personajes y una interfaz

---
+++ interfaz.c
@@ -0,0 +1,222 @@
+#include "interfaz.h"
+#include "gimnasio.h"
+#include "personaje.h"
+#include "heap.h"
+
+typedef struct juego{
+    heap_t* gimnasios;
+    gimnasio_t* gimnasio_actual;
+    personaje_t* personaje;
+    bool simular;
+}juego_t;
+
+
+void menu_inicio_header(){
+    printf("###############################################################################################\n");
+    printf("#                                                                                             #\n");
+    printf("#                                         MENU INICIO                                         #\n");
+    printf("#                                                                                             #\n");
+    printf("###############################################################################################\n");
+    printf("#                                                                                             #\n");
+    printf("#                                                                                             #\n");
+    printf("#                                                                                             #\n");
+    printf("#                                                                                             #\n");
+    printf("#                                                                                             #\n");
+    printf("#                                                                                             #\n");
+    printf("#                                                                                             #\n");
+    printf("#                                                                                             #\n");
+    printf("#                                                                                             #\n");
+    printf("#                                                                                             #\n");
+    printf("#                                                                                             #\n");
+    printf("#                                                                                             #\n");
+    printf("#                                                                                             #\n");
+
+}
+
+/*
+ * E: Ingresa el archivo del entrenador principal.
+ * A: Agrega un gimnasio al árbol de gimnasios.
+ * I: Comienza la partida.
+ * S: Simula la partida.
+*/
+void mostrar_menu_inicio(juego_t* juego){
+    system("clear");
+
+    menu_inicio_header();
+
+    printf("#                                                                                             #\n");
+    printf("#   Presione:                                                                                 #\n");
+    printf("#                                                                                             #\n");
+    printf("#   ");
+    if(juego->personaje){
+        printf("- 'E' para Ingresar Entrenador Principal");
+    }else{
+        printf("- 'E' para Ingresar Entrenador Principal");
+    }
+    printf("              - 'A' para Agregar un Gimnasio      #\n");
+
+    // TODO: Cambiar color cuando la partida sea valida
+    printf("#   - 'I' Comenzar la partida                             - 'S' Simular la Partida            #\n");
+    printf("#                                                                                             #\n");
+    printf("###############################################################################################\n");
+    printf("#                                                                                             #\n");
+
+
+}
+
+void info_cargar_personaje(){
+    system("clear");
+    menu_inicio_header();
+
+    printf("###############################################################################################\n");
+    printf("#                                                                                             #\n");
+    printf("#   Esta Ingresando el Entrenador Principal recuerde:                                         #\n");
+    printf("#      - El archivo tiene que estar en Archivos/Personaje                                     #\n");
+    printf("#      - El archivo tiene que tener el formato que se encuentra en el README                  #\n");
+    printf("#      - Solo puede cargar un Entrenador Principal por partida                                #\n");
+    printf("#                                                                                             #\n");
+    printf("###############################################################################################\n");
+    printf("#                                                                                             #\n");
+}
+
+
+
+void info_cargar_gimnasio(){
+    system("clear");
+    menu_inicio_header();
+
+    printf("###############################################################################################\n");
+    printf("#                                                                                             #\n");
+    printf("#   Esta Ingresando uno o varios Gimnasios:                                                   #\n");
+    printf("#      - El archivo tiene que estar en Archivos/Gimnasios                                     #\n");
+    printf("#      - El archivo tiene que tener el formato que se encuentra en el README                  #\n");
+    printf("#      - Puede haber uno o mas gimnasios en el archivo                                        #\n");
+    printf("#      - Todos los gimnasios tienen que tener un lider con Pokemon                            #\n");
+    printf("#                                                                                             #\n");
+    printf("###############################################################################################\n");
+    printf("#                                                                                             #\n");
+}
+
+/*
+ * E: Muestra al entrenador principal junto a sus Pokémon.
+ * G: Muestra la información del gimnasio actual.
+ * C: Permite cambiar los Pokémon de batalla.
+ * B: Realiza la próxima batalla planificada.
+ *      - Si se derrotó al líder del gimnasio se deberá mostrar el Menú de victoria
+ *      - Si se sufre una derrota frente a cualquier entrenador se deberá mostrar el Menú de derrot
+*/
+void mostrar_menu_gimnasio(juego_t* juego){
+    printf("###############################################################################################\n");
+    printf("#                                                                                             #\n");
+    printf("#                            %37s                            #\n",juego->gimnasio_actual->nombre);
+    printf("#                                                                                             #\n");
+    printf("###############################################################################################\n");
+    printf("#                                                                                             #\n");
+    printf("#                                                                                             #\n");
+    printf("#                                                                                             #\n");
+    printf("#                                                                                             #\n");
+    printf("#                                                                                             #\n");
+    printf("#                                                                                             #\n");
+    printf("#                                                                                             #\n");
+    printf("#                                                                                             #\n");
+    printf("#                                                                                             #\n");
+    printf("#                                                                                             #\n");
+    printf("#                                                                                             #\n");
+    printf("#   Ingrese:                                                                                  #\n");
+    printf("#                                                                                             #\n");
+    printf("#   - 'T' Tomar Pokemon del Lider                         - 'C' Cambiar Pokemon               #\n");
+    printf("#   - 'N' Pelear el Proximo Gimnasio                                                          #\n");
+    printf("#                                                                                             #\n");
+    printf("###############################################################################################\n");
+    printf("#                                                                                             #\n");
+}
+
+/*
+ * Aquí se mostrará la información de la batalla, Pokémon vs Pokémon.
+ * Deberá ser informativa: mostrar cuál es el Pokémon del rival y cuál es el tuyo, sus características y el resultado del
+ * combate.
+ * 
+ * N: Próximo combate.
+*/
+void mostrar_menu_batalla(){
+
+}
+
+
+/*
+ * C: Permite cambiar los Pokémon de batalla.
+ * R: Reintenta el gimnasio.
+ * F: Finaliza la partida.
+*/
+void mostrar_menu_derrota(){
+    printf("###############################################################################################\n");
+    printf("#                                                                                             #\n");
+    printf("#                                         MENU DERROTA                                        #\n");
+    printf("#                                                                                             #\n");
+    printf("###############################################################################################\n");
+    printf("#                                                                                             #\n");
+    printf("#                                                                                             #\n");
+    printf("#                                                                                             #\n");
+    printf("#                                                                                             #\n");
+    printf("#                                                                                             #\n");
+    printf("#                                                                                             #\n");
+    printf("#                                                                                             #\n");
+    printf("#                                                                                             #\n");
+    printf("#                                                                                             #\n");
+    printf("#                                                                                             #\n");
+    printf("#                                                                                             #\n");
+    printf("###############################################################################################\n");
+    printf("#                                                                                             #\n");
+    printf("#   Su Party ha sido derrotada, pero eso no significa que usted haya perdido. Usted puede     #\n");
+    printf("#   cambiar su Pokemon y reintentar el gimnasio, derrotar al entrenador que te gano y seguir  #\n");
+    printf("#   tu aventura para converitrte en un Maestro Pokemon                                        #\n");
+    printf("#                                                                                             #\n");
+    printf("#   Ingrese:                                                                                  #\n");
+    printf("#                                                                                             #\n");
+    printf("#   - 'C' Cambiar Pokemon                                                                     #\n");
+    printf("#   - 'R' Reintentar el gimnasio                            - 'F' Finalizar la Partida        #\n");
+    printf("#                                                                                             #\n");
+    printf("###############################################################################################\n");
+    printf("#                                                                                             #\n");
+}
+
+
+/*
+ * T: Toma un Pokémon del líder y lo incorpora en los Pokémon obtenidos del jugador.
+ *       - Una vez realizada dicha operación, esta opción desaparece del menú.
+ * C: Permite cambiar los Pokémon de batalla.
+ * N: Próximo gimnasio.
+ *       - Si no hay próximo gimnasio, se deberá notificar al usuario que se convirtió en Maestro Pokémon.
+*/
+void mostrar_menu_victoria(){
+    printf("###############################################################################################\n");
+    printf("#                                                                                             #\n");
+    printf("#                                         MENU VICTORIA                                       #\n");
+    printf("#                                                                                             #\n");
+    printf("###############################################################################################\n");
+    printf("#                                                                                             #\n");
+    printf("#                                                                                             #\n");
+    printf("#                                                                                             #\n");
+    printf("#                                                                                             #\n");
+    printf("#                                                                                             #\n");
+    printf("#                                                                                             #\n");
+    printf("#                                                                                             #\n");
+    printf("#                                                                                             #\n");
+    printf("#                                                                                             #\n");
+    printf("#                                                                                             #\n");
+    printf("#                                                                                             #\n");
+    printf("#                                                                                             #\n");
+    printf("#                                                                                             #\n");
+    printf("#                                                                                             #\n");
+    printf("#   Presione:                                                                                 #\n");
+    printf("#                                                                                             #\n");
+    printf("#   - 'T' Tomar Pokemon del Lider                         - 'C' Cambiar Pokemon               #\n");
+    printf("#   - 'N' Pelear el Proximo Gimnasio                                                          #\n");
+    printf("#                                                                                             #\n");
+    printf("###############################################################################################\n");
+    printf("#                                                                                             #\n");
+}
+
+void pantalla_maestro_pokemon(){
+
+}
---
+++ interfaz.h
@@ -0,0 +1,32 @@
+#ifndef __INTERFAZ_H__
+#define __INTERFAZ_H__
+
+#include <stdio.h>
+
+
+void mostrar_menu_inicio();
+
+
+void info_cargar_personaje();
+
+void mostrar_error_carga_personaje();
+
+
+void info_cargar_gimnasio();
+
+
+void mostrar_menu_gimnasio();
+
+
+void mostrar_menu_batalla();
+
+
+void mostrar_menu_derrota();
+
+
+void mostrar_menu_victoria();
+
+
+void pantalla_maestro_pokemon();
+
+#endif // __INTERFAZ_H__
